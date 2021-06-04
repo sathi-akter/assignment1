5bcd027f45578ec49d817ede0acb5fa74ac5a5aa
Sathi Akter <sathi.akter1@hotmail.com>
2021-06-04 08:10:08 -0400

    added explanation for the app.js video

--- app.js
+++ app.js
@@ -1,27 +1,34 @@
+// requiring the needed libraries
 var createError = require("http-errors");
 var express = require("express");
 var path = require("path");
 var cookieParser = require("cookie-parser");
 var logger = require("morgan");
 
+// set where the routes are and how to access them
 var indexRouter = require("./routes/index");
 
+// instantiate the express application
 var app = express();
 
 // view engine setup
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "ejs");
 
+// not sure
 app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
+
 // serve static files
 app.use(express.static(path.join(__dirname, "public")));
+// serve styles
 app.use(
   "/css",
   express.static(path.join(__dirname + "node_modules/bootstrap/dist/css"))
 );
+// serve js
 app.use(
   "/js",
   express.static(path.join(__dirname + "node_modules/bootstrap/dist/js"))
